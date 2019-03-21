8981c37b68059e27696ab4f947cfde5403bcd991
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-03-21 17:04:34 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace liketocode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Do you like to program? ");
+            string anwser = Console.ReadLine();
+
+            if (anwser == "yes")
+            {
+                Console.WriteLine("Fun to hear :-).");
+            }
+            else
+            {
+                Console.WriteLine("Fight anyway!");
+            }
+            Console.ReadLine();
+            //If the answer is "Yes", the program should show: "Fun to hear :-)."
+            //Otherwise, the program should show: "Fight anyway!"
+        }
+    }
+}
